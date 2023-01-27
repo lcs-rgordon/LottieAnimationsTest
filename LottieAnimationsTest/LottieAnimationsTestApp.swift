b50ac55b7629b00db75c076c2f43cc8002f8260f
Russell Gordon <rgordon@lcs.on.ca>
2023-01-27 12:37:17 -0500

    Initial Commit

---
+++ LottieAnimationsTest/LottieAnimationsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsTestApp.swift
+//  LottieAnimationsTest
+//
+//  Created by Russell Gordon on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
